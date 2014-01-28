00ad84b3f785ae99411ddf7ae36cfdd882739e2a
sepna <le.saaleeh.mon@gmail.com>
2014-01-29 01:38:11 +0330

    Update ckeditor.lang.php

---
+++ inc/languages/persian/ckeditor.lang.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Persian Language Pack
+ * Copyright 2014 My-BB.Ir Group, All Rights Reserved
+ * Update : چهارشنبه 9 بهمن 1392 - ساعت 1:30 صبح
+ */
+
+$l['mybbirckeditor_lang'] = 'fa';
+$l['mybbirckeditor_dir'] = 'rtl';
+$l['mybbirckeditor_xxsmall'] = '١';
+$l['mybbirckeditor_xsmall'] = '٢';
+$l['mybbirckeditor_small'] = '٣';
+$l['mybbirckeditor_medium'] = '٤';
+$l['mybbirckeditor_large'] = '٥';
+$l['mybbirckeditor_xlarge'] = '٦';
+$l['mybbirckeditor_xxlarge'] = '٧';
+$l['mybbirckeditor_moresmilies'] = 'لیست شکلک ها';
+$l['mybbirckeditor_autosavesaved'] = 'متن به صورت خودکار ذخیره شد';
+$l['mybbirckeditor_restoreautosave'] = 'بازگردانی متن';
+$l['mybbirckeditor_previewautosave'] = 'پیش‌نمایش متن';
+$l['mybbirckeditor_removeautosave'] = 'حذف متن';
+$l['mybbirckeditor_autosavefound'] = 'یک ذخیره‌ی خودکار یافت شد.';
+$l['mybbirckeditor_userckeditor'] = 'استفاده از ویرایشگر پیشرفته (CKEditor)';
+//$l['mybbirckeditor_'] = '';
+?>
