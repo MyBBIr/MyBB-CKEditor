840230e235ff33c56c367ea5676aacfb1b0980a5
ATofighi <alirtofighim@gmail.com>
2015-09-01 16:45:13 +0430

    Version 1.0.0 xD

--- inc/plugins/ckeditor/info.php
+++ inc/plugins/ckeditor/info.php
@@ -16,7 +16,7 @@ function ckeditor_info()
 		"website"       => "https://github.com/ATofighi/MyBB-CKEditor",
 		"author"        => "AliReza_Tofighi",
 		"authorsite"    => "http://my-bb.ir",
-		"version"       => "0.8.1",
+		"version"       => "1.0.0",
 		"compatibility" => "18*"
 	);
 	if(ckeditor_is_installed() && $plugins_cache['active']['ckeditor'])
@@ -24,6 +24,23 @@ function ckeditor_info()
 		global $PL;
 		$PL or require_once PLUGINLIBRARY;
 		$info["description"] .= "<br /><a href=\"index.php?module=config/settings&action=change&search=ckeditor_\">{$lang->changesettings}</a>.";
+		
+		$functionphpContent = file_get_contents(MYBB_ROOT.'inc/functions.php');
+		if(!strstr($functionphpContent, 'return ckeditor_build($bind, $smilies);')) {
+			// TODO: localization
+			$info["description"] .= '<br />Install is not complete. please open inc/functions.php file, find:<pre style="direction:ltr;text-align:left;background:#f7f7f7;border:1px solid #ccc;border-radius: 3px;padding:10px;display:block;width:auto">function build_mycode_inserter($bind="message", $smilies = true)
+{
+	global $db, $mybb, $theme, $templates, $lang, $plugins, $smiliecache, $cache;
+</pre> and replace to:<pre style="direction:ltr;text-align:left;background:#f7f7f7;border:1px solid #ccc;border-radius: 3px;padding:10px;display:block;width:auto">
+function build_mycode_inserter($bind="message", $smilies = true)
+{
+/* - PL:ckeditor - /* 	global $db, $mybb, $theme, $templates, $lang, $plugins, $smiliecache, $cache;
+/* + PL:ckeditor + */ if(function_exists("ckeditor_build") && is_ckeditor_avilable())
+/* + PL:ckeditor + */ {
+/* + PL:ckeditor + */ return ckeditor_build($bind, $smilies);
+/* + PL:ckeditor + */ }
+/* + PL:ckeditor + */ global $db, $mybb, $theme, $templates, $lang, $plugins, $smiliecache, $cache;</pre>';
+		}
 	}
 	return $info;
 }
